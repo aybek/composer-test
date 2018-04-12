88fb570e91a4931db9d32241d2b11cd70585d28e
aibek-genesiszero <aybek@genesiszero.com>
2018-04-13 02:13:43 +0600

    first commit

---
+++ test/test.js
@@ -0,0 +1,187 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+const path = require('path');
+
+require('chai').should();
+
+const NS = 'org.acme.bank.test';
+
+describe('BankTrade', () => {
+    // In-memory card store for testing so cards are not persisted to the file system
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+    let adminConnection;
+    let businessNetworkConnection;
+
+    before(async () => {
+        // Embedded connection used for local testing
+        const connectionProfile = {
+            name: 'embedded',
+            'x-type': 'embedded'
+        };
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
+
+        // PeerAdmin identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+
+        const deployerCardName = 'PeerAdmin';
+        adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
+    });
+
+    beforeEach(async () => {
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+
+        const adminUserName = 'admin';
+        let adminCardName;
+        let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+
+        // Install the Composer runtime for the new business network
+        await adminConnection.install(businessNetworkDefinition);
+
+        // Start the business network and configure an network admin identity
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: adminUserName,
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkDefinition.getName(), businessNetworkDefinition.getVersion(), startOptions);
+
+        // Import the network admin identity for us to use
+        adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`;
+        await adminConnection.importCard(adminCardName, adminCards.get(adminUserName));
+
+        // Connect to the business network using the network admin identity
+        await businessNetworkConnection.connect(adminCardName);
+    });
+
+    describe('#makeRequest', () => {
+
+        it('should add the request to requestListing', async () => {
+
+            const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+            const seller = factory.newResource(NS, 'Trader', '111');
+            seller.firstName = 'Seller';
+            seller.lastName = 'Selman';
+
+            const buyer = factory.newResource(NS, 'Trader', '222');
+            buyer.firstName = 'Buyer';
+            buyer.lastName = 'Buyman';
+
+            const bank = factory.newResource(NS, 'Bank', '333');
+            bank.name = 'Kazkom';
+            bank.address = 'abaya 33';
+
+            const sellerAccount = factory.newResource(NS,'BankAccount' ,'444');
+            sellerAccount.owner = factory.newRelationship(NS, 'Trader', seller.$identifier);
+            sellerAccount.balance = 0;
+
+            const buyerAccount = factory.newResource(NS,'BankAccount', '555');
+            buyerAccount.owner = factory.newRelationship(NS, 'Trader', buyer.$identifier);
+            buyerAccount.balance = 1000;
+
+            const product = factory.newResource(NS,'Product', '444');
+            product.seller = factory.newRelationship(NS, 'Trader', seller.$identifier);
+            product.sellerAccount = factory.newRelationship(NS, 'BankAccount', sellerAccount.$identifier);
+            product.price = 10;
+
+        
+            const listing = factory.newResource(NS, 'RequestList', 'LISTING_001');
+
+          
+            const request = factory.newTransaction(NS, 'Request');
+            offer.product = factory.newRelationship(NS, 'Product', product.$identifier);
+            offer.buyerAccount = factory.newRelationship(NS, 'BankAccount', 'LISTING_001');
+        
+
+            // Get the registries.
+            const productRegistry = await businessNetworkConnection.getAssetRegistry(NS + '.Product');
+            const requestListingRegistry = await businessNetworkConnection.getAssetRegistry(NS + '.RequestListing');
+            const traderRegistry = await businessNetworkConnection.getParticipantRegistry(NS + '.Trader');
+            const bankRegistry = await businessNetworkConnection.getParticipantRegistry(NS + '.Bank');
+
+            
+            await productRegistry.add(product);
+
+            
+            await requestListingRegistry.add(listing);
+
+    
+            await traderRegistry.addAll([buyer, seller]);
+
+    
+            await bankRegistry.addAll([bank]);
+
+            
+            await businessNetworkConnection.submitTransaction(request);
+
+            
+            const req1 = factory.newTransaction(NS, 'Request');
+                offer.product = factory.newRelationship(NS, 'Product', product.$identifier);
+                offer.buyerAccount = factory.newRelationship(NS, 'BankAccount', 'LISTING_001');
+                await businessNetworkConnection.submitTransaction(req1);
+
+        
+            let newListing = await requestListingRegistry.get(listing.$identifier);
+
+        
+            newListing.requests.length.should.equal(2);
+
+            
+            const commit = factory.newTransaction(NS, 'CommitRequest');
+            commit.listing = factory.newRelationship(NS, 'RequestListing', 'LISTING_001');
+            await businessNetworkConnection.submitTransaction(commit);
+
+        
+            newListing = await requestListingRegistry.get(listing.$identifier);
+
+
+            newListing.state.should.equal('SOLD');
+
+            
+            const theBuyer = await traderRegistry.get(buyer.$identifier);
+            const theSeller = await traderRegistry.get(seller.$identifier);
+
+            
+            theBuyer.balance.should.equal(800);
+
+            
+            theSeller.balance.should.equal(200);
+
+            
+            const product1 = await productRegistry.get(product.$identifier);
+
+            
+            product1.owner.getIdentifier().should.equal(buyer.$identifier);
+        });
+    });
+});
